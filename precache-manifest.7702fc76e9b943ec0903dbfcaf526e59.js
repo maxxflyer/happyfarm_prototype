de6cdd88676860a78653648cf720beb2f58c0d66
maxxflyer <maxxflyer@gmail.com>
2020-09-14 14:26:57 +0200

    Add files via upload

---
+++ precache-manifest.7702fc76e9b943ec0903dbfcaf526e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872efe26cfaa8486959d1da0c12c0e44",
+    "url": "/happyfarm/index.html"
+  },
+  {
+    "revision": "f09c10f34ab54e04c9da",
+    "url": "/happyfarm/static/css/main.74e6690f.chunk.css"
+  },
+  {
+    "revision": "55a41280f5c8da6fd61e",
+    "url": "/happyfarm/static/js/2.536da0bf.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/happyfarm/static/js/2.536da0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09c10f34ab54e04c9da",
+    "url": "/happyfarm/static/js/main.248d8996.chunk.js"
+  },
+  {
+    "revision": "be03aa12d03bc93d3ebc",
+    "url": "/happyfarm/static/js/runtime-main.0896056b.js"
+  }
+]);
