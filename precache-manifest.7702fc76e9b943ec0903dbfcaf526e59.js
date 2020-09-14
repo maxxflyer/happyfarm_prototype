0d7f7c349432cf73e8bea22fe0d5535f6e9c4238
maxxflyer <maxxflyer@gmail.com>
2020-09-14 14:40:14 +0200

    Update precache-manifest.7702fc76e9b943ec0903dbfcaf526e59.js

--- precache-manifest.7702fc76e9b943ec0903dbfcaf526e59.js
+++ precache-manifest.7702fc76e9b943ec0903dbfcaf526e59.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "872efe26cfaa8486959d1da0c12c0e44",
-    "url": "/happyfarm/index.html"
+    "url": "/happyfarm_prototype/index.html"
   },
   {
     "revision": "f09c10f34ab54e04c9da",
-    "url": "/happyfarm/static/css/main.74e6690f.chunk.css"
+    "url": "/happyfarm_prototype/static/css/main.74e6690f.chunk.css"
   },
   {
     "revision": "55a41280f5c8da6fd61e",
-    "url": "/happyfarm/static/js/2.536da0bf.chunk.js"
+    "url": "/happyfarm_prototype/static/js/2.536da0bf.chunk.js"
   },
   {
     "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
-    "url": "/happyfarm/static/js/2.536da0bf.chunk.js.LICENSE.txt"
+    "url": "/happyfarm_prototype/static/js/2.536da0bf.chunk.js.LICENSE.txt"
   },
   {
     "revision": "f09c10f34ab54e04c9da",
-    "url": "/happyfarm/static/js/main.248d8996.chunk.js"
+    "url": "/happyfarm_prototype/static/js/main.248d8996.chunk.js"
   },
   {
     "revision": "be03aa12d03bc93d3ebc",
